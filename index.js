0f0c49bdd899e76a55f703f4c572c138d69d4a30
DIMAS <dimasna@365.telkomuniversity.ac.id>
2020-02-13 08:42:34 +0700

    a

--- index.js
+++ index.js
@@ -37,15 +37,15 @@ app.post('/callback', line.middleware(config), (req, res) => {
 
   // req.body.events should be an array of events
   if (!Array.isArray(req.body.events)) {
-    return res.status(200).end();
+    return res.status(500).end();
   }
 
   // handle events separately
   Promise.all(req.body.events.map(handleEvent))
-    .then(() => res.status(200).end())
+    .then(() => res.end())
     .catch((err) => {
       console.error(err);
-      res.status(200).end();
+      res.status(500).end();
     });
 });
 
