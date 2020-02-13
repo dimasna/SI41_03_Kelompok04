6654230355785b7194bd6467df2a7e00c0cc599d
DIMAS <dimasna@365.telkomuniversity.ac.id>
2020-02-13 08:59:01 +0700

    a

--- index.js
+++ index.js
@@ -533,7 +533,7 @@ function handleEvent(event) {
       }
 
     case 'follow':
-      return client.replyMessage(event.replyToken, ['a',onboard]);
+      return client.replyMessage(event.replyToken, onboard);
 
     case 'unfollow':
       return console.log(`Unfollowed this bot: ${JSON.stringify(event)}`);
