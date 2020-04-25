4546ce6e3ef986ed4ddcff4d9a84d35b0efdc16e
adindainezsang <adindainezsang@gmail.com>
2020-04-25 21:15:55 +0700

    a

---
+++ app/src/main/java/tech/ezapp/ezadmin/TransaksiDetailTotalWithdraw.java
@@ -0,0 +1,14 @@
+package tech.ezapp.ezadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransaksiDetailTotalWithdraw extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaksi_detail_total_withdraw);
+    }
+}
---
+++ app/src/main/java/tech/ezapp/ezadmin/TransaksiDetailWithdrawSukses.java
@@ -0,0 +1,14 @@
+package tech.ezapp.ezadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransaksiDetailWithdrawSukses extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaksi_detail_withdraw_sukses);
+    }
+}
