fa7bd48c72a7b5bc85bd8d83fe2e840715b13212
Dimas NA <dimasna96@gmail.com>
2020-04-25 18:24:00 +0800

    inez

---
+++ app/src/main/java/tech/ezapp/ezadmin/TopUpAdapter.java
@@ -0,0 +1,73 @@
+package tech.ezapp.ezadmin;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+
+import tech.ezapp.ezadmin.dummy.DummyContent.DummyItem;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
+ * specified {@link }.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class TopUpAdapter extends RecyclerView.Adapter<TopUpAdapter.ViewHolder> {
+
+    private final List<DummyItem> mValues;
+
+    public TopUpAdapter(List<DummyItem> items) {
+        mValues = items;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.topup_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public DummyItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mIdView = (TextView) view.findViewById(R.id.item_number);
+            mContentView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
