fb0dfa6bc2a134c072115256c558d1ce07073777
adindainezsang <adindainezsang@gmail.com>
2020-04-25 12:29:41 +0700

    a

---
+++ app/src/main/java/tech/ezapp/ezadmin/TransaksiKonfirmasiTopUp.java
@@ -0,0 +1,14 @@
+package tech.ezapp.ezadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TransaksiKonfirmasiTopUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaksi_konfirmasi_top_up);
+    }
+}
